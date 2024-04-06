7e875004995c7b02bd07683f9051606e644a5a08
LeeMichael <87360548+mocchen@users.noreply.github.com>
2024-04-07 05:24:25 +0800

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# 定义颜色变量
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+cop_info(){
+    clear
+    echo -e "${GREEN}######################################
+    #          ${RED}Debian换源 一键脚本           ${GREEN}#
+    #             作者: ${YELLOW}末晨             ${GREEN}#
+    ######################################${NC}"
+    echo
+}
+
 # 检查用户是否为root
 if [ "$(id -u)" != "0" ]; then
-    echo "该脚本必须以root身份运行。"
+    echo -e "${RED}该脚本必须以root身份运行。${NC}"
     exit 1
 fi
 
@@ -13,13 +28,13 @@ cp /etc/apt/sources.list /etc/apt/sources.list.backup
 update_sources() {
     # 检查网络连接是否正常
     if ! ping -c 1 deb.debian.org &> /dev/null; then
-        echo "无法连接到官方Debian镜像源。请检查您的网络连接。"
+        echo -e "${RED}无法连接到官方Debian镜像源。请检查您的网络连接。${NC}"
         exit 1
     fi
 
     # 检查官方Debian源是否可用
     if ! curl -s --head https://deb.debian.org/debian/ | head -n 1 | grep "200 OK" > /dev/null; then
-        echo "官方Debian镜像源不可用。请稍后再试或选择其他镜像源。"
+        echo -e "${RED}官方Debian镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
         exit 1
     fi
 
@@ -36,14 +51,14 @@ deb-src https://deb.debian.org/debian/ bullseye-backports main contrib non-free
 deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 deb-src https://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 EOF
-    echo "Debian源已成功更新为官方镜像。"
+    echo -e "${GREEN}Debian源已成功更新为官方镜像。${NC}"
 }
 
 # 更新为清华镜像的函数
 update_tsinghua_mirrors_sources() {
     # 检查清华镜像源是否可用
     if ! curl -s --head https://mirrors.tuna.tsinghua.edu.cn/debian/ | head -n 1 | grep "200 OK" > /dev/null; then
-        echo "清华镜像源不可用。请稍后再试或选择其他镜像源。"
+        echo -e "${RED}清华镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
         exit 1
     fi
 
@@ -60,14 +75,14 @@ deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main con
 deb https://mirrors.tuna.tsinghua.edu.cn/debian-security/ bullseye-security main contrib non-free
 deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security/ bullseye-security main contrib non-free
 EOF
-    echo "Debian源已成功更新为使用清华镜像。"
+    echo -e "${GREEN}Debian源已成功更新为使用清华镜像。${NC}"
 }
 
 # 更新为中科大镜像的函数
 update_ustc_mirrors_sources() {
     # 检查中科大镜像源是否可用
     if ! curl -s --head https://mirrors.ustc.edu.cn/debian/ | head -n 1 | grep "200 OK" > /dev/null; then
-        echo "中科大镜像源不可用。请稍后再试或选择其他镜像源。"
+        echo -e "${RED}中科大镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
         exit 1
     fi
 
@@ -84,14 +99,14 @@ deb-src https://mirrors.ustc.edu.cn/debian/ bullseye-backports main contrib non-
 deb https://mirrors.ustc.edu.cn/debian-security/ bullseye-security main contrib non-free
 deb-src https://mirrors.ustc.edu.cn/debian-security/ bullseye-security main contrib non-free
 EOF
-    echo "Debian源已成功更新为使用中科大镜像。"
+    echo -e "${GREEN}Debian源已成功更新为使用中科大镜像。${NC}"
 }
 
 # 更新为腾讯云镜像的函数
 update_tencent_mirrors_sources() {
     # 检查腾讯云镜像源是否可用
     if ! curl -s --head https://mirrors.cloud.tencent.com/debian/ | head -n 1 | grep "200 OK" > /dev/null; then
-        echo "腾讯云镜像源不可用。请稍后再试或选择其他镜像源。"
+        echo -e "${RED}腾讯云镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
         exit 1
     fi
 
@@ -103,19 +118,19 @@ deb https://mirrors.cloud.tencent.com/debian/ bullseye-updates main contrib non-
 deb-src https://mirrors.cloud.tencent.com/debian/ bullseye-updates main contrib non-free
 
 deb https://mirrors.cloud.tencent.com/debian/ bullseye-backports main contrib non-free
-deb-src https://mirrors.cloud.tencent.com/debian/ bullseye-backports main contrib non-free
+deb-src https://mirrors.cloud.tencent.com/debian/ bullseye-backports maincontrib non-free
 
 deb https://mirrors.cloud.tencent.com/debian-security/ bullseye-security main contrib non-free
 deb-src https://mirrors.cloud.tencent.com/debian-security/ bullseye-security main contrib non-free
 EOF
-    echo "Debian源已成功更新为使用腾讯云镜像。"
+    echo -e "${GREEN}Debian源已成功更新为使用腾讯云镜像。${NC}"
 }
 
 # 更新为阿里云镜像的函数
 update_aliyun_mirrors_sources() {
     # 检查阿里云镜像源是否可用
     if ! curl -s --head https://mirrors.aliyun.com/debian/ | head -n 1 | grep "200 OK" > /dev/null; then
-        echo "阿里云镜像源不可用。请稍后再试或选择其他镜像源。"
+        echo -e "${RED}阿里云镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
         exit 1
     fi
 
@@ -132,8 +147,10 @@ deb-src https://mirrors.aliyun.com/debian/ bullseye-backports main contrib non-f
 deb https://mirrors.aliyun.com/debian-security/ bullseye-security main contrib non-free
 deb-src https://mirrors.aliyun.com/debian-security/ bullseye-security main contrib non-free
 EOF
-    echo "Debian源已成功更新为使用阿里云镜像。"
+    echo -e "${GREEN}Debian源已成功更新为使用阿里云镜像。${NC}"
 }
+
+cop_info
 
 echo "请选择一个选项："
 echo "0: 退出"
@@ -147,7 +164,7 @@ read -p "请输入您的选择：" choice
 # 检查用户输入并相应地更新源列表
 case "$choice" in
     0)
-        echo "退出，未更改源。"
+        echo -e "${RED}退出，未更改源。${NC}"
         exit 0
         ;;
     1)
@@ -166,7 +183,7 @@ case "$choice" in
         update_aliyun_mirrors_sources
         ;;
     *)
-        echo "无效的选项，请选择0、1、2、3、4或5。"
+        echo -e "${RED}无效的选项，请选择0、1、2、3、4或5。${NC}"
         exit 1
         ;;
 esac
