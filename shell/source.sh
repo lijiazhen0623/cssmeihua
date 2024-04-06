dabe26f34ce95d3e66b9c9feacc6f46df28a3c3a
LeeMichael <87360548+mocchen@users.noreply.github.com>
2024-04-07 06:13:05 +0800

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -22,10 +22,14 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # 备份现有的sources.list
-cp /etc/apt/sources.list /etc/apt/sources.list.backup
+backup_sources() {
+    cp /etc/apt/sources.list /etc/apt/sources.list.backup
+}
 
 # 更新为官方Debian镜像源的函数
 update_sources() {
+    backup_sources
+    
     # 检查官方Debian镜像源是否可用
     if ! curl -s --head https://deb.debian.org/debian/ | head -n 1 | grep "200" > /dev/null; then
         echo -e "${RED}官方Debian镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
@@ -50,6 +54,8 @@ EOF
 
 # 更新为清华镜像的函数
 update_tsinghua_mirrors_sources() {
+    backup_sources
+
     # 检查清华镜像源是否可用
     if ! curl -s --head https://mirrors.tuna.tsinghua.edu.cn/debian/ | head -n 1 | grep "200" > /dev/null; then
         echo -e "${RED}清华镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
@@ -74,6 +80,8 @@ EOF
 
 # 更新为中科大镜像的函数
 update_ustc_mirrors_sources() {
+    backup_sources
+    
     # 检查中科大镜像源是否可用
     if ! curl -s --head https://mirrors.ustc.edu.cn/debian/ | head -n 1 | grep "200" > /dev/null; then
         echo -e "${RED}中科大镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
@@ -98,6 +106,8 @@ EOF
 
 # 更新为腾讯云镜像的函数
 update_tencent_mirrors_sources() {
+    backup_sources
+    
     # 检查腾讯云镜像源是否可用
     if ! curl -s --head https://mirrors.cloud.tencent.com/debian/ | head -n 1 | grep "200" > /dev/null; then
         echo -e "${RED}腾讯云镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
@@ -122,6 +132,8 @@ EOF
 
 # 更新为阿里云镜像的函数
 update_aliyun_mirrors_sources() {
+    backup_sources
+    
     # 检查阿里云镜像源是否可用
     if ! curl -s --head https://mirrors.aliyun.com/debian/ | head -n 1 | grep "200" > /dev/null; then
         echo -e "${RED}阿里云镜像源不可用。请稍后再试或选择其他镜像源。${NC}"
@@ -144,6 +156,16 @@ EOF
     echo -e "${GREEN}Debian源已成功更新为使用阿里云镜像。${NC}"
 }
 
+# 恢复备份的源函数
+restore_sources() {
+    if [ -f /etc/apt/sources.list.backup ]; then
+        cp /etc/apt/sources.list.backup /etc/apt/sources.list
+        echo -e "${GREEN}已成功恢复为备份的源。${NC}"
+    else
+        echo -e "${RED}无法检测到备份文件，请检查是否有备份。${NC}"
+    fi
+}
+
 cop_info
 
 echo "请选择一个选项："
@@ -153,6 +175,7 @@ echo "2: 使用清华镜像源"
 echo "3: 使用中科大镜像源"
 echo "4: 使用腾讯云镜像源"
 echo "5: 使用阿里云镜像源"
+echo "6: 恢复备份的源"
 read -p "请输入您的选择：" choice
 
 # 检查用户输入并相应地更新源列表
@@ -175,6 +198,9 @@ case "$choice" in
         ;;
     5)
         update_aliyun_mirrors_sources
+        ;;
+    6)
+        restore_sources
         ;;
     *)
         echo -e "${RED}无效的选项，请选择0、1、2、3、4或5。${NC}"
